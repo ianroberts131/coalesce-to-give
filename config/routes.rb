21849ab5f3fd0c8f3e87f7b33df05b73711ba66e
ianroberts131 <ianroberts131@gmail.com>
2016-06-22 04:40:49 +0000

    Format sign up page, fix routes, and fix feature spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   
-  root 'welcome#home'
-  
   devise_for :users
   
-  authenticated :user do
-    root to: 'welcome#home'
-  end
+  # authenticated :user do
+  #   root to: 'welcome#home'
+  # end
+  
+  root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/features/user_sign_ups_spec.rb
+++ spec/features/user_sign_ups_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "User Sign Up", type: :feature do
     scenario 'successfully' do
         visit '/users/sign_up'
-        expect(page).to have_css('h2', text: 'Sign Up')
+        expect(page).to have_css('h2', text: 'New User Sign Up')
         fill_in 'First name', with: 'Ian'
         fill_in 'Last name', with: 'Roberts'
         fill_in 'Email', with: 'ianroberts131@gmail.com'
@@ -11,7 +11,7 @@ RSpec.feature "User Sign Up", type: :feature do
         fill_in('Password confirmation', with: 'password', exact: true)
         click_button 'Sign up'
         
-        expect(page).to have_text "Welcome Ian!"
+        expect(page).to have_text "Welcome"
     end
     
 end
