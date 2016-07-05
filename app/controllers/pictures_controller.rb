c9841daee4374df0eb2381ba558feb4917763537
ianroberts131 <ianroberts131@gmail.com>
2016-07-05 04:54:12 +0000

    Add ability to upload pictures to Nonprofits, fix pictures controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,41 @@
 class PicturesController < ApplicationController
   def index
-    @user = current_user
+    
+    if params[:user_id].present?
+      if current_user
+        @user = current_user
+      else
+        @user = User.find(params[:user_id])
+      end
+      @pictures = @user.pictures.all
+    else
+      @nonprofit = Nonprofit.find(params[:nonprofit_id])
+      @pictures = @nonprofit.pictures.all
+    end
+    
     @picture = Picture.new
-    @pictures = @user.pictures.all
   end
 
   def new
     @user = current_user
-    @picture = @user.pictures.new
+    
+    if params[:user_id].present?
+      @picture = @user.pictures.new
+    else
+      @nonprofit = @user.nonprofits.find(params[:nonprofit_id])
+      @picture = @nonprofit.pictures.new
+    end
   end
 
   def create
     @user = current_user
-    @picture = @user.pictures.new(picture_params)
+    
+    if params[:user_id].present?
+      @picture = @user.pictures.new(picture_params)
+    else
+      @nonprofit = @user.nonprofits.find(params[:nonprofit_id])
+      @picture = @nonprofit.pictures.new(picture_params)
+    end
     
     if @picture.save
       flash[:notice] = "Picture uploaded successfully"
@@ -20,7 +43,11 @@ class PicturesController < ApplicationController
       flash[:alert] = "There was an error uploading the picture. Please try again"
     end
     
-    redirect_to user_pictures_path
+    if params[:user_id].present?
+      redirect_to user_pictures_path
+    else
+      redirect_to nonprofit_pictures_path
+    end
   end
   
   private
